1a91d0a2a42ea416b52149bc1cf86f69f9934883
mayag <maya.g@apiiro.com>
2023-08-09 12:36:33 +0300

    3 Servers are talking to each other - frontend, api-gateway wnd web. currently only get is working, missing the db

--- docker/api-gateway/server.js
+++ docker/api-gateway/server.js
@@ -1,18 +1,41 @@
 const express = require('express');
+const httpProxy = require('http-proxy');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost'); // Replace with your frontend's URL
+  res.header('Access-Control-Allow-Origin', 'http://localhost');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
+// Create a proxy server
+const proxy = httpProxy.createProxyServer();
+
+// Define routes to forward requests to the web service
 app.get('/', (req, res) => {
-  res.send('Hello from the API Gateway!');
+  proxy.web(req, res, { target: 'http://web:5000' });
+});
+
+app.post('/new-post', (req, res) => {
+  proxy.web(req, res, { target: 'http://web:5000' });
+});
+
+app.delete('/', (req, res) => {
+  proxy.web(req, res, { target: 'http://web:5000' });
+});
+
+
+// Handle errors from the proxy server
+proxy.on('error', (err, req, res) => {
+  console.error('Proxy error:', err);
+  res.status(500).send('Proxy error');
 });
 
 app.listen(port, () => {
-  console.log(`API Gateway listening at <http://localhost:${port}>`)
+  console.log(`API Gateway listening on port ${port}`);
 });
--- docker/flask/app.py
+++ docker/flask/app.py
@@ -6,9 +6,25 @@ api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        return {'message': 'Hello World'}
+        return {'message': 'Web server is up'}
+
+class NewPost(Resource):
+    def post(self):
+        data = request.get_json()
+        user = data.get('user')
+        content = data.get('content')
+        # Process user and content data
+        return {'user': user, 'content': content}
+
+class DeleteResource(Resource):
+    def delete(self):
+        id = request.args.get('id')
+        # Delete resource with specified ID
+        return {'message': f'Deleted resource with ID {id}'}
 
 api.add_resource(HelloWorld, '/')
+api.add_resource(NewPost, '/new-post')
+api.add_resource(DeleteResource, '/')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
