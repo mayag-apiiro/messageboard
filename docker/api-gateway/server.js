5623e35f3d60a8a142948c1d0c752936240fe099
mayag <maya.g@apiiro.com>
2023-08-13 16:02:53 +0300

    4 Services are working properly, with get and post

--- docker/api-gateway/server.js
+++ docker/api-gateway/server.js
@@ -1,41 +1,18 @@
-const express = require('express');
+const http = require('http');
 const httpProxy = require('http-proxy');
-const bodyParser = require('body-parser');
-const app = express();
-const port = 3000;
 
-app.use(bodyParser.json());
+const proxy = httpProxy.createProxyServer({target: "http://web:5000", changeOrigin: true});
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+const server = http.createServer((req, res) => {
+  // Set up CORS headers here
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // Proxy the request to the target server
+  proxy.web(req, res);
 });
 
-// Create a proxy server
-const proxy = httpProxy.createProxyServer();
-
-// Define routes to forward requests to the web service
-app.get('/', (req, res) => {
-  proxy.web(req, res, { target: 'http://web:5000' });
-});
-
-app.post('/new-post', (req, res) => {
-  proxy.web(req, res, { target: 'http://web:5000' });
-});
-
-app.delete('/', (req, res) => {
-  proxy.web(req, res, { target: 'http://web:5000' });
-});
-
-
-// Handle errors from the proxy server
-proxy.on('error', (err, req, res) => {
-  console.error('Proxy error:', err);
-  res.status(500).send('Proxy error');
-});
-
-app.listen(port, () => {
-  console.log(`API Gateway listening on port ${port}`);
+server.listen(3000, () => {
+  console.log('Proxy server listening on port 3000');
 });
--- docker/flask/app.py
+++ docker/flask/app.py
@@ -1,20 +1,92 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
+import psycopg2
+
 
 app = Flask(__name__)
 api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        return {'message': 'Web server is up'}
+        try:
+            # Connect to the PostgreSQL database
+            connection = psycopg2.connect(
+                user="postgres",
+                password="1234",
+                host="db",  # This corresponds to the service name in Docker Compose
+                port="5432",
+                database="postgres"
+            )
+            cursor = connection.cursor()
+
+            # Retrieve all rows from the table
+            select_query = "SELECT * FROM messages"
+            cursor.execute(select_query)
+            rows = cursor.fetchall()
+
+            # Close the database connection
+            cursor.close()
+            connection.close()
+
+            # Convert rows to a list of dictionaries
+            message_list = []
+            for row in rows:
+                message = {
+                    'user': row[0],
+                    'title': row[1],
+                    'content': row[2]
+                }
+                message_list.append(message)
+
+            return jsonify({'messages': message_list})
+
+        except:
+            app.logger.exception("Something went wrong while fetching messages")
+            return jsonify({'error': 'Failed to fetch messages'}), 500
 
 class NewPost(Resource):
     def post(self):
+        app.logger.info("HAJABABABEKKKOOO!")
+        try:
+            app.logger.info("Entire request object:")
+            app.logger.info(request.__dict__)
+            app.logger.info("Fetching json")
+            app.logger.info(request.json)
+            app.logger.info(request)
+            app.logger.info(request.get_json())
+            
+        except:
+            app.logger.exception("Something went wrong")
+        
+        app.logger.info("Fetching json after")
+        # return jsonify({'message': 'Message added successfully'})
+        request.json
         data = request.get_json()
         user = data.get('user')
+        title = data.get('title')
         content = data.get('content')
-        # Process user and content data
-        return {'user': user, 'content': content}
+        # app.logger.info(content)
+        app.logger.info("V1")
+
+
+        # Connect to the PostgreSQL database
+        connection = psycopg2.connect(
+            user="postgres",
+            password="1234",
+            host="db",  # This corresponds to the service name in Docker Compose
+            port="5432",
+            database="postgres"
+        )
+        cursor = connection.cursor()
+
+        # Insert the new message into the database
+        insert_query = "INSERT INTO messages (\"user\", title, content) VALUES (%s, %s, %s)"
+        cursor.execute(insert_query, (user, title, content))
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+        return jsonify({'message': 'Message was added successfully'})
 
 class DeleteResource(Resource):
     def delete(self):
